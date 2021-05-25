c4aaee2fbea608a675236e9d50d82a581666408c
Darlan Thiago <tdarlan1@hotmail.com>
2021-05-25 20:13:29 -0300

    Add Mod11 Algo

--- regex.php
+++ regex.php
@@ -8,6 +8,30 @@ if (!empty($_POST)) {
   $cpf = addslashes($_POST['cpf']);
   $email = addslashes($_POST['email']);
 
+  function mod11($numDado, $numDig = 2, $limMult = 12, $x10 = true)
+  {
+
+    if (!$x10) $numDig = 1;
+    $dado = $numDado;
+    for ($n = 1; $n <= $numDig; $n++) {
+      $soma = 0;
+      $mult = 2;
+      for ($i = strlen($dado) - 1; $i >= 0; $i--) {
+        $soma += $mult * intval(substr($dado, $i, 1));
+        if (++$mult > $limMult) $mult = 2;
+      }
+      if ($x10) {
+        $dig = fmod(fmod(($soma * 10), 11), 10);
+      } else {
+        $dig = fmod($soma, 11);
+        if ($dig == 10) $dig = "X";
+      }
+      $dado .= strval($dig);
+    }
+    return substr($dado, strlen($dado) - $numDig);
+  }
+
+
 
   function validate($placa_carro, $cpf, $email)
   {
@@ -22,10 +46,18 @@ if (!empty($_POST)) {
 
     if (strlen($cpf) >= 11 || strlen($cpf) <= 14) {
 
-      $cpf_formatted = preg_replace('/^([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{2})$/', '$1.$2.$3-$4', $cpf);
+      $cpf_formatted = preg_replace('/[^0-9]/', '', $cpf);
 
-      if (preg_match('/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-([0-9]{2})$/', $cpf_formatted)) {
+      $cpf_split = str_split($cpf_formatted);
 
+      $digit = $cpf_split[9] . $cpf_split[10];
+
+      $cpf_to_validade = array_slice($cpf_split, 0, 9);
+      $cpf_to_validade = (string) implode($cpf_to_validade);
+
+      $validated = mod11($cpf_to_validade);
+
+      if ($validated === $digit) {
         $messages['cpf'] = 1;
       } else {
         $messages['cpf'] = 0;
